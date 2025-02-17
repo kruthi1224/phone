7529b331d3f5314bd14362ef66f35ba46044bbc6
kruthika gowda sk <kruthikask2003@gmail.com>
2025-02-17 13:51:36 +0530

    Update App.java

--- src/main/java/flipkart/App.java
+++ src/main/java/flipkart/App.java
@@ -5,6 +5,6 @@ package flipkart;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! hi");
     }
 }
